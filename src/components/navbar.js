af1e33da12644c1e4c0ab4f18520fc2183681b51
allisonlu8 <98787033+allisonlu8@users.noreply.github.com>
2023-11-17 10:04:00 +0100

    tweaks to header

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,5 +1,6 @@
 // Add this script in the head of your HTML or in a separate scripts.js file
   // scripts.js
+  /*
 document.addEventListener("DOMContentLoaded", function () {
     // Smooth scrolling for navigation links
     document.querySelectorAll('.navbar a').forEach(anchor => {
@@ -10,7 +11,7 @@ document.addEventListener("DOMContentLoaded", function () {
         const sectionId = this.getAttribute('href').substring(1);
   
         // Check if the sectionId matches a specific page
-        if (sectionId === 'Journey' || sectionId === 'Adventure' || sectionId === 'About') {
+        if (sectionId === 'journey' || sectionId === 'adventure' || sectionId === 'about') {
           // Redirect to the corresponding page
           window.location.href = `${sectionId}.html`;
         } else {
@@ -24,7 +25,64 @@ document.addEventListener("DOMContentLoaded", function () {
         }
       });
     });
-  
+  */
+ // navbar.js
+ /*document.addEventListener("DOMContentLoaded", function () {
+    // Get the current page filename
+    var currentPage = window.location.pathname.split("/").pop();
+
+    // Remove the file extension from the filename
+    currentPage = currentPage.replace(".html", "");
+
+    // Add the 'active' class to the corresponding navigation link
+    var navLinks = document.querySelectorAll("nav a");
+    navLinks.forEach(function (link) {
+        var linkPage = link.href.split("/").pop().replace(".html", "");
+        if (linkPage === currentPage) {
+            link.classList.add("active");
+        }
+
+        // Add click event listener to handle navigation
+        link.addEventListener("click", function (event) {
+            event.preventDefault(); // Prevent the default behavior of the link
+
+            // Get the target page from the link's href
+            var targetPage = this.href.split("/").pop().replace(".html", "");
+
+            // Redirect the user to the target page
+            window.location.href = "../src/pages/" + targetPage + ".html";
+        });
+    });
+});*/
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Get the current page filename
+    var currentPage = window.location.pathname.split("/").pop();
+
+    // Remove the file extension from the filename
+    currentPage = currentPage.replace(".html", "");
+
+    // Add the 'active' class to the corresponding navigation link
+    var navLinks = document.querySelectorAll("nav a");
+    navLinks.forEach(function (link) {
+        var linkPage = link.href.split("/").pop().replace(".html", "");
+        if (linkPage === currentPage) {
+            link.classList.add("active");
+        }
+
+        // Add click event listener to handle navigation
+        link.addEventListener("click", function (event) {
+            event.preventDefault(); // Prevent the default behavior of the link
+
+            // Get the target page from the link's href
+            var targetPage = this.href.split("/").pop().replace(".html", "");
+
+            // Redirect the user to the target page
+            window.location.href = "../pages/" + targetPage + ".html";
+        });
+    });
+
+
     // Dropdown functionality
     const dropdownBtn = document.querySelector('.dropbtn');
     const dropdownContent = document.querySelector('.dropdown-content');
