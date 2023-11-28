d505c5e64abb383c6d9416ad226fd47e41420e0e
allisonlu8 <98787033+allisonlu8@users.noreply.github.com>
2023-11-28 11:54:27 +0100

    add a slider for photos instead

--- src/components/script.js
+++ src/components/script.js
@@ -113,6 +113,43 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
+
+// for the slider photos
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("slide");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+
+function nextSlide() {
+    plusSlides(1);
+}
+
+function prevSlide() {
+    plusSlides(-1);
+}
+
 /*
 document.addEventListener("DOMContentLoaded", function () {
     // Get the current page filename
