de61b225b8fc0fee1d66c160fb09beb24f0f36af
allisonlu8 <98787033+allisonlu8@users.noreply.github.com>
2023-11-28 11:46:44 +0100

    adjust style, js, and html for specific pages respectively

--- src/components/script.js
+++ src/components/script.js
@@ -63,6 +63,56 @@ document.addEventListener("DOMContentLoaded", function () {
         });
 });
 
+// components/script.js
+document.addEventListener("DOMContentLoaded", function () {
+    var currentPage = window.location.pathname.split("/").pop().replace(".html", "");
+    var pageTitles = {
+        "journey": "Journey",
+        "adventure": "Adventure",
+        "movement": "Movement",
+        "documentation": "Documentation",
+        "illustration": "Illustration",
+        "about": "About",
+        // Add more pages as needed
+    };
+
+    // Set the page title and description
+    document.getElementById("page-title").innerText = pageTitles[currentPage] || "Unknown Page";
+    document.getElementById("page-description").innerText = "Your personalized side description goes here.";
+
+    // Get page-specific information from meta tags
+    var pageTitle = document.querySelector('meta[name="title"]').getAttribute("content") || "Unknown Page";
+    var pageDescription = document.querySelector('meta[name="description"]').getAttribute("content") || "Your personalized side description goes here.";
+
+    // Set the page title and description
+    document.getElementById("page-title").innerText = pageTitle;
+    document.getElementById("page-description").innerText = pageDescription;
+
+});
+
+
+/*For section selection in adventure */
+// script.js
+document.addEventListener('DOMContentLoaded', function() {
+    var navLinks = document.querySelectorAll('nav a');
+
+    navLinks.forEach(function(link) {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+
+            var targetId = this.getAttribute('href').substring(1);
+            var targetSection = document.getElementById(targetId);
+
+            if (targetSection) {
+                targetSection.scrollIntoView({
+                    behavior: 'smooth',
+                    block: 'start'
+                });
+            }
+        });
+    });
+});
+
 /*
 document.addEventListener("DOMContentLoaded", function () {
     // Get the current page filename
