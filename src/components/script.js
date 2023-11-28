853023bd1350c9b170abcc89d346e2abffc69e91
allisonlu8 <98787033+allisonlu8@users.noreply.github.com>
2023-11-28 14:08:25 +0100

    adjust sliders

--- src/components/script.js
+++ src/components/script.js
@@ -178,37 +178,8 @@ function createSlider(sliderId, slidePrefix, imageCount) {
     };
 }
 
-// Create two sliders
-var slider1 = createSlider("slider1", "slide1_", 2);
-var slider2 = createSlider("slider2", "slide2_", 2);
+// Create sliders
+var slider1 = createSlider("slider1", "slide1_", 3);
+var slider2 = createSlider("slider2", "slide2_", 3);
+var slider3 = createSlider("slider3", "slide3_", 3);
 
-
-/*
-document.addEventListener("DOMContentLoaded", function () {
-    // Get the current page filename
-    var currentPage = window.location.pathname.split("/").pop();
-
-    // Remove the file extension from the filename
-    currentPage = currentPage.replace(".html", "");
-
-    // Add the 'active' class to the corresponding navigation link
-    var navLinks = document.querySelectorAll("nav a");
-    navLinks.forEach(function (link) {
-        var linkPage = link.href.split("/").pop().replace(".html", "");
-        if (linkPage === currentPage) {
-            link.classList.add("active");
-        }
-
-        // Add click event listener to handle navigation
-        link.addEventListener("click", function (event) {
-            event.preventDefault(); // Prevent the default behavior of the link
-
-            // Get the target page from the link's href
-            var targetPage = this.href.split("/").pop().replace(".html", "");
-
-            // Redirect the user to the target page
-            window.location.href = "../pages/" + targetPage + ".html";
-        });
-    });
-});
-*/
